40660cd4406e3651a6d7a6bfd7a381d43d16bdf8
J4y_boi <57105859+j4y-boi@users.noreply.github.com>
2025-08-15 15:53:23 +0200

    finished cars
    good enough for this game jam also added signs, fences and fixed hitboxes

--- scripts/car.gd
+++ scripts/car.gd
@@ -18,17 +18,21 @@ const startRot = [
 var current = 0
 var randomOffset = 0
 var stop = false
+var counter = 0
+
+var rng = RandomNumberGenerator.new()
 
 func randint(minimu:int, maximu:int) -> int: #im used to python :P
-	var rng = RandomNumberGenerator.new()
 	return rng.randi_range(minimu, maximu)
 
 func newPos():
 	current = 1 - current
-	randomOffset = randint(0,4)
+	if randint(1,1) == 1:
+		randomOffset = randint(30,40)
+	else:
+		randomOffset = randint(0,4)
 	car.position = startPos[current]
 	car.rotation = startRot[current]
-	var rng = RandomNumberGenerator.new()
 	var randomColor = Color(0,0,0)
 	while randomColor == Color(0,0,0):
 		randomColor = Color(rng.randi_range(0,1), rng.randi_range(0,1), rng.randi_range(0,1))
@@ -39,7 +43,7 @@ func newPos():
 		mat.set_shader_parameter("midColor", randomColor)
 		mat.set_shader_parameter("highColor", Color(1,1,1))
 
-	mat = car_back.material_override # I HATE SHADERS WHO THE FUCK NEEDS THEM ANYWAY
+	mat = car_back.material_override
 	if mat and mat is ShaderMaterial:
 		mat.set_shader_parameter("lowColor", Color(0,0,0))
 		mat.set_shader_parameter("midColor", randomColor)
@@ -49,26 +53,34 @@ func newPos():
 	stop = false
 
 func respawn():
+	counter = 0
+	stop = true
 	await get_tree().create_timer(randint(1,5)).timeout
 	newPos()
 
 func _process(delta: float) -> void:
 	if not stop:
+		counter += 1
 		car.translate(Vector3(0,0,(SPEED + randomOffset) * delta))
+		# in case we miss hitbox
+		var target_z = startPos[1 - current].z
+		if (current == 0 and car.global_transform.origin.z >= target_z) or (current == 1 and car.global_transform.origin.z <= target_z):
+			respawn()
+		if counter >= 600: #in case the in case we miss hitbox fails
+			respawn()
 
 func _on_tunnel_1_area_entered(_area: Area3D) -> void:
 	if current == 1 and not stop:
-		stop = true
 		respawn()
 
 func _on_tunnel_2_area_entered(_area: Area3D) -> void:
 	if current == 0 and not stop:
-		stop = true
 		respawn()
 
 func _ready() -> void:
-	current = randint(0,1)
+	current = int(car.name.left(4))%2
 	car.position = startPos[current]
 	car.rotation = startRot[current]
 	stop = true
+	await get_tree().create_timer(rng.randf_range(0.00,1.00)).timeout
 	respawn()
