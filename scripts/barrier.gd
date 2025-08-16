8e49d8ab7e8cb26270604dc3a59be3efd9896dc0
J4y_boi <57105859+j4y-boi@users.noreply.github.com>
2025-08-16 02:10:03 +0200

    cam menu
    added functionality for some buttons, working on getting stats to work

--- scripts/barrier.gd
+++ scripts/barrier.gd
@@ -1,12 +1,17 @@
 extends Node3D
-var is_up = false
+var is_up = true
+@onready var gate_closer: Timer = $gateCloser
 
 func raise():
-	if not is_up:
-		$AnimationPlayer.play("up")
-		is_up = true
+	$AnimationPlayer.play("up")
+	is_up = true
+	gate_closer.stop()
 
 func lower():
-	if is_up:
-		$AnimationPlayer.play("down")
-		is_up = false
+	$AnimationPlayer.play("down")
+	is_up = false
+	gate_closer.start()
+
+
+func _on_gate_closer_timeout() -> void:
+	raise()
--- scripts/cams.gd
+++ scripts/cams.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var cam1: SubViewport = $HBoxContainer/camcontainer1/cam1
 @onready var camcontainer_1: SubViewportContainer = $HBoxContainer/camcontainer1
+@onready var fg: ColorRect = $ColorRect2
 
 var currentCam = 1
 var game_scene = preload("res://scenes/game.tscn")
@@ -11,10 +12,29 @@ func _ready():
 	cam1.add_child(scene1)
 	scene1.get_node("lantern/Camera3D2").current = true
 
-func _on_switch_button_up() -> void:
+func _on_switch_pressed() -> void:
 	if currentCam == 1:
 		currentCam = 2
 		scene1.get_node("lantern2/Camera3D").current = true
 	else:
 		currentCam = 1
 		scene1.get_node("lantern/Camera3D2").current = true
+
+func _on_close_pressed() -> void:
+	var is_up = scene1.get_node("barrier").is_up
+	var animationPlayer = scene1.get_node("barrier/AnimationPlayer")
+	if not animationPlayer.current_animation != "":
+		if is_up:
+			scene1.get_node("barrier").lower()
+		else:
+			scene1.get_node("barrier").raise()
+
+func _on_exit_pressed() -> void:
+	fg.modulate.a = 0.0
+	fg.visible = true
+	$leave.play()
+	var tween = self.create_tween()
+	tween.tween_property(fg, "modulate:a", 1.0, 1)
+	tween.tween_interval(.5)
+	await tween.finished
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
